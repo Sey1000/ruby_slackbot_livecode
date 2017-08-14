a36a06b57d90d523fce17422c31433ae4d87517d
Sey Cheon <22985121+Sey1000@users.noreply.github.com>
2017-08-14 17:41:45 +0200

    Update Gemfile

--- play.rb
+++ play.rb
@@ -1,5 +1,9 @@
 require 'http'
 require 'json'
 
-rc = HTTP.post("https://slack.com/api/api.test")
+rc = HTTP.post("https://slack.com/api/chat.postMessage", params: {
+  token: ENV['SLACK_API_TOKEN'],
+  channel: '#general',
+  text: 'Frederike is not me'
+  })
 puts JSON.pretty_generate(JSON.parse(rc.body))
